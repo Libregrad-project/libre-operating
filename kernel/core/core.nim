d18d5babd45f8ef1fe1918d3b3bbe00542cd75f4
ternicinc <allexander.linux@gmail.com>
2025-08-08 20:34:48 +0200

    Information

--- kernel/core/core.nim
+++ kernel/core/core.nim
@@ -1,2 +1,4 @@
 # TODO: We need to make the kernel system, which can include / import external Assembly (ASM) code, 
 # Which then will be used inside the code-base.
+
+# To import Assembly in Nim: {.importasm: "boot/boot.asm";}.
