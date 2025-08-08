72a7fd8d3199f1d6667717eb5d868b4da81ff1d5
ternicinc <allexander.linux@gmail.com>
2025-08-08 20:50:19 +0200

    Study comments

--- kernel/core/core.nim
+++ kernel/core/core.nim
@@ -1,4 +1,22 @@
 # TODO: We need to make the kernel system, which can include / import external Assembly (ASM) code, 
 # Which then will be used inside the code-base.
 
-# To import Assembly in Nim: {.importasm: "boot/boot.asm";}.
+# Declare the external kernel_main symbol from the boot.o file. Which we get with:
+# nasm -f elf64 -o boot.o boot.asm
+
+#Since kernel_main is an external function declared in the assembly code, 
+# we will need to provide a simple C header file (boot.h) that declares this external function.
+
+# Example:
+# // boot.h
+# void kernel_main(void); 
+
+# And then nim c -d:nodejs --link:boot.o core.nim 
+
+#proc kernel_main() {.importc: "kernel_main", cdecl, header: "<boot.h>".}
+
+proc initKernel() =
+    echo "Kernel Initialized."
+ #   kernel_main() 
+
+initKernel()
