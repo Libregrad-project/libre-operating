6ff14b17d8682a1dc9c54a8c0352520af0ee8796
ternicinc <allexander.linux@gmail.com>
2025-08-08 20:25:33 +0200

    Nim Config

--- config/config.nim
+++ config/config.nim
@@ -1,3 +1,7 @@
 const
     OS_NAME = "Libre Operating"
     VERSION = "0.0.1"
+	DEBUG_MODE = true
+	
+# const 
+    # TODO: Add more definitions, for example: Our file system declarations and limits.
