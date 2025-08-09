3a4719a3b684f133a2d88858a9c94ee92f260f97
ternicinc <allexander.linux@gmail.com>
2025-08-09 14:54:51 +0200

    Cleaner

---
+++ obj/src/halt.c.d
@@ -0,0 +1,1 @@
+obj/src/halt.c.o: src/halt.c
--- src/main.c
+++ src/main.c
@@ -4,17 +4,11 @@
 #include <limine.h>
 #include "font8x8.h"
 #include "graphics.h"
-// Set the base revision to 3, this is recommended as this is the latest
-// base revision described by the Limine boot protocol specification.
-// See specification for further info.
 
 __attribute__((used, section(".limine_requests")))
 static volatile LIMINE_BASE_REVISION(3);
 
-// The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, usually, they should
-// be made volatile or equivalent, _and_ they should be accessed at least
-// once or marked as used with the "used" attribute as done here.
+
 
 __attribute__((used, section(".limine_requests")))
 static volatile struct limine_framebuffer_request framebuffer_request = {
@@ -22,8 +16,7 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
     .revision = 0
 };
 
-// Finally, define the start and end markers for the Limine requests.
-// These can also be moved anywhere, to any .c file, as seen fit.
+
 
 __attribute__((used, section(".limine_requests_start")))
 static volatile LIMINE_REQUESTS_START_MARKER;
@@ -31,11 +24,6 @@ static volatile LIMINE_REQUESTS_START_MARKER;
 __attribute__((used, section(".limine_requests_end")))
 static volatile LIMINE_REQUESTS_END_MARKER;
 
-// GCC and Clang reserve the right to generate calls to the following
-// 4 functions even if they are not directly called.
-// Implement them as the C specification mandates.
-// DO NOT remove or rename these functions, or stuff will eventually break!
-// They CAN be moved to a different .c file.
 
 void *memcpy(void *dest, const void *src, size_t n) {
     uint8_t *pdest = (uint8_t *)dest;
@@ -88,15 +76,13 @@ int memcmp(const void *s1, const void *s2, size_t n) {
     return 0;
 }
 
-// Halt and catch fire function.
 static void hcf(void) {
     for (;;) {
         asm ("hlt");
     }
 }
 
-// The following will be our kernel's entry point.
-// If renaming kmain() to something else, make sure to change the
+
 void kmain(void) {
     if (LIMINE_BASE_REVISION_SUPPORTED == false) hcf();
     if (!framebuffer_request.response || framebuffer_request.response->framebuffer_count < 1) hcf();
