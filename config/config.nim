f8e6fb86c3f738a88e473a91e8f7f10c613595ac
ternicinc <allexander.linux@gmail.com>
2025-08-09 10:48:29 +0200

    Test.nim changes.

--- config/config.nim
+++ config/config.nim
@@ -1,7 +1,7 @@
 const
     OS_NAME = "Libre Operating"
     VERSION = "0.0.1"
-	DEBUG_MODE = true
-	
+    DEBUG_MODE = true
+
 # const 
     # TODO: Add more definitions, for example: Our file system declarations and limits.
--- tests/test.nim
+++ tests/test.nim
@@ -1,27 +1,6 @@
 include ../config/config
 
-echo "Hello, Nim on Windows 7!"
+proc printInfo() =
+    echo OS_NAME
 
-# In memory pointer.
-var x = 42
-var ptrX: ptr int = addr x
-# Print the pointer. [] de-references it.
-echo ptrX[]
-
-# Global accessable variable.
-var globalVariable = 50
-
-proc printGlobal() =
-    echo globalVariable
-
-proc greet(name: cstring) =
-    echo "Hello, ", name, "!"
-
-proc runTest() =
-  echo "OS Name: ", OS_NAME
-  echo "Version: ", VERSION
-
-runTest()
-
-greet("Libre Operating")
-printGlobal()
+printInfo()
