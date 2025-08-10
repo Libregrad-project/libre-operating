ce46837aea3d8adaf85a0e3c04138eeba621f0b7
ternicinc <allexander.linux@gmail.com>
2025-08-10 11:00:26 +0200

    Initial

---
+++ sys/sys/Parameters.h
@@ -0,0 +1,31 @@
+#ifndef _SYS_PARAMS_H_
+#define _SYS_PARAMS_H_
+
+// Version of the operating system.
+#define LBR_VRS       001
+// Future release form.
+#define LBR_FRF       002
+
+
+//////////////////////////
+// Systsem limits
+#define MAX_USERS     120
+#define MAX_GROUPS    200
+#define HOSTNAME_LEN  256
+#define NOFILE        65536
+// Max processes per user.
+#define PROC_USERM    1000
+// Max threads per process.
+#define MAX_PTHREADS  1000
+// Max sockets per process.
+#define MAX_PSOCKS    1000
+//////////////////////////
+//////////////////////////
+// Blockchain Limits
+/////////////////////////
+// Max Blockchain Peers
+#define MAX_BPEERS   100
+// Max inbound peers.
+#define MAX_IPEERS   50
+// Max outbound peers.
+#define MAX_OPEERS   50
